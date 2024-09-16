c6a916d158ba0b5c0df7ea25ee133b17b4ec06a2
Sambit Sahoo <soulsam480@hotmail.com>
2024-09-17 01:07:35 +0530

    build: fix build from type error

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,6 +1,5 @@
 import { init, mdToHtml } from "md4w";
 import wasmUrl from "md4w/js/md4w-small.wasm?url";
-import { INote } from "./note";
 
 export async function initMarkdownWasm() {
   return await init(wasmUrl);
